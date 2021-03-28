6042c485876139f950289cc9a9f35d685815ffb3
vUnAmp <phanhaingoc@gmail.com>
2021-03-28 09:24:02 +0200

    theming for consistently and using fonts

--- App.tsx
+++ App.tsx
@@ -2,17 +2,44 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
-// import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components";
+import { theme } from './src/infrastructure/theme'
+
+// Fonts 
+import {
+  useFonts as useLato,
+  Lato_400Regular,
+} from '@expo-google-fonts/lato';
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from '@expo-google-fonts/oswald';
 
 
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar'
 
 import { RestaurantsScreen } from './src/features/restaurants/screens/restaurant.screen'
 
+
+
 export default function App() {
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  })
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  })
+  if (!latoLoaded || !oswaldLoaded) {
+    return null
+  }
+
+
+
   return (
     <>
-      <RestaurantsScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
       <ExpoStatusBar style='auto' />
     </>
   );
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Integration styled-components with React Native using Typescript :
 
 > npm @types/styled-components-react-native -D
+> need using parenthese styled(<Component>) instead of styled.<Component> to avoid error `Property 'space' does not exist on type 'DefaultTheme'` typescript
